4a6a177abfffd9852f7d9d85832041dda2d87c9b
Asoji <asoji-git@hiitsdevin.mozmail.com>
2024-11-10 11:07:39 -0800

    🚧 wip Enso drpc [it is NOT going well AT ALL]

--- DiscordRichPresence.cs
+++ DiscordRichPresence.cs
@@ -1,5 +1,8 @@
-﻿using DiscordRPC;
+﻿using System;
+using DiscordRPC;
 using DiscordRPC.Logging;
+using Il2CppInterop.Runtime;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using LogLevel = DiscordRPC.Logging.LogLevel;
 
@@ -90,7 +93,8 @@ public class DiscordRichPresence {
         UpdatePresence();
     }
 
-    public void SongSelectScreenRpc(bool trainingMode) { // make this an enum switch like below to support several different modes
+    public void SongSelectScreenRpc(bool trainingMode) {
+        // make this an enum switch like below to support several different modes
         richPresence.Details = "Song Select";
 
         if (trainingMode)
@@ -112,11 +116,11 @@ public class DiscordRichPresence {
         DonChanBand
     }
 
-    public void EnsoScreenRpc(EnsoType ensoType) {
+    public void EnsoScreenRpc(EnsoType ensoType, String songNamePassthrough, EnsoData.EnsoLevelType ensoDifficulty) {
         switch (ensoType) {
             case EnsoType.Normal:
-                richPresence.Details = PlaceholderText.ENSO_SONG_NAME;
-                richPresence.State = PlaceholderText.ENSO_DIFFICULTY;
+                richPresence.Details = songNamePassthrough;
+                richPresence.State = ensoDifficulty.ToString();
                 richPresence.Assets = new DiscordRPC.Assets() {
                     LargeImageKey = "logo",
                     LargeImageText = "Taiko"
@@ -183,11 +187,68 @@ public class DiscordRichPresence {
                 break;
             case "Enso":
                 Plugin.Log.LogInfo("Currently banging a drum as hard as humanly possible");
-                EnsoScreenRpc(EnsoType.Normal);
+
+                // klhdtakl;bl;ksftbiotjb oh my god i cant believe this fuckin try catch finally actually works
+                // and that i didnt have to do hacky shit to retrieve from the DontDestroyOnLoad scene because
+                // this is exactly where this manager lives
+                var ensoDataManager = GameObject.Find("CommonObjects/Datas/EnsoDataManager").GetComponent<EnsoDataManager>();
+                var ensoLevelDifficulty = ensoDataManager.GetDiffCourse(0);
+
+                try {
+                    Scene ensoActiveScene = SceneManager.GetActiveScene();
+                    Plugin.Log.LogInfo(ensoActiveScene.name); // ???
+
+                    // Plugin.Log.LogInfo(ensoActiveScene.GetRootGameObjects()[0].GetComponent<SongInfoPlayer>()); // ???? [loud incorrect buzzer]
+
+                    // var ensoSongName = GameObject.Find("CanvasFg/SongInfo").GetComponent<SongInfoPlayer>()
+                    //     .GetSongName(ensoDataManager.ensoSettings.musicuid); ????????
+                    // Plugin.Log.LogInfo(ensoSongName); // [loud incorrect buzzer x 2]
+
+                    // var ensoSongName = GameObject.Find("CanvasFg/SongInfo").GetComponent<SongInfoPlayer>().m_SongName;
+                    // Plugin.Log.LogInfo(ensoSongName); // ???????? [loud incorrect buzzer x 3]
+
+                    // this is not going well.
+
+                    var isTheFuckingCanvasFgActive = GameObject.Find("CanvasFg").active;
+                    Plugin.Log.LogInfo($"Is CanvasFg active? {isTheFuckingCanvasFgActive}");
+
+                    if (isTheFuckingCanvasFgActive) { // if you think this is a horrible if-else chain dont worry it gets worse
+                        Plugin.Log.LogInfo("Okay, the CanvasFg is active. Now, is the SongInfo active?");
+
+                        var isTheFuckingCanvasFgSongInfoActive = GameObject.Find("SongInfo").active;
+                        if (isTheFuckingCanvasFgSongInfoActive) {
+                            Plugin.Log.LogInfo("Okay, the SongInfo is active. Then what is going on...?"); // turns out SongInfoPlayer is null ya numbskull, but how to deal with that, i dont know.
+                            // Plugin.Log.LogInfo("Okay, lets now try to fetch SongInfoPlayer's active state");
+
+                            // var songInfoPlayerState = GameObject.Find("SongInfo").GetComponentInChildren<SongInfoPlayer>(); // it NREs and idk how to deal with that
+                            // while (songInfoPlayerState == null) { // why did i think this wasnt going to freeze the game OFC IT'S GOING TO FREEZE THE FUCKING GAME
+                            //     Plugin.Log.LogInfo("Waiting for song info player state...");
+                            // }
+                            // Plugin.Log.LogInfo($"Oh hello!: {songInfoPlayerState.m_SongName}");
+                        }
+                        else {
+                            Plugin.Log.LogInfo("Fuck. Nope, SongInfo isn't active.");
+                        }
+                    }
+                    else {
+                        Plugin.Log.LogInfo("Fuck. Nope, CanvasFg isn't active.");
+                    }
+                }
+                catch (Il2CppException exception) {
+                    Plugin.Log.LogError($"SongInfo could not be found, or some other error occured, {exception.Message}");
+                }
+                finally {
+                    // Plugin.Log.LogInfo("oh shit what x 2");
+                    // Plugin.Log.LogInfo(
+                    //     $"What's the song name Don-chan??? {GameObject.Find("CanvasFg/SongInfo").GetComponent<SongInfoPlayer>()
+                    //         .GetSongName(ensoDataManager.ensoSettings.musicuid)}");
+                }
+
+                EnsoScreenRpc(EnsoType.Normal, PlaceholderText.ENSO_SONG_NAME, ensoLevelDifficulty);
                 break;
             case "EnsoTrainingFull":
                 Plugin.Log.LogInfo("Currently practicing banging the drums as hard as humanly possible");
-                EnsoScreenRpc(EnsoType.Training);
+                // EnsoScreenRpc(EnsoType.Training);
                 break;
             case "OnlineRoom":
                 Plugin.Log.LogInfo("Currently on Online Room screen");
@@ -215,7 +276,7 @@ public class DiscordRichPresence {
                 break;
             case "EnsoDonChanBand":
                 Plugin.Log.LogInfo("Currently playing for the Don Chan Band");
-                EnsoScreenRpc(EnsoType.DonChanBand);
+                // EnsoScreenRpc(EnsoType.DonChanBand);
                 break;
             case "MyRoom":
                 Plugin.Log.LogInfo("GET THE FUCK OUT OF MY ROOM IM PLAYING MINECRAFT");
@@ -243,7 +304,7 @@ public class DiscordRichPresence {
                 break;
             case "EnsoScenario":
                 Plugin.Log.LogInfo("Currently progressing");
-                EnsoScreenRpc(EnsoType.Scenario);
+                // EnsoScreenRpc(EnsoType.Scenario);
                 break;
             default:
                 Plugin.Log.LogError(
---
+++ Utils.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+namespace TNTRFDRPC.Utils;
+
+public class Utils {
+
+}
